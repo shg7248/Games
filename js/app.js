2d03c88a372a34f8f64385c8d657ff7d8da8f0d4
Song <shg7248@gmail.com>
2023-05-31 14:47:45 +0900

    기능수정

--- js/app.js
+++ js/app.js
@@ -14,10 +14,14 @@ import Board from './board.js';
 import Piece from './piece.js';
 
 function App() {
+
+    this.started = false;
+
     this.canvas = document.querySelector('#app');
     this.context = this.canvas.getContext('2d');
 
     this.startBtn = document.querySelector('#start');
+    this.score_ = document.querySelector('#score');
 
     this.context.canvas.width = WIDTH;
     this.context.canvas.height = HEIGHT;
@@ -26,25 +30,29 @@ function App() {
 
     this.time = {start: 0, elapsed: 0, level: 500};
 
-    this.board = new Board(this.context);
+    this.board = new Board(this);
     this.piece = new Piece(this.context);
 
     this.startBtn.addEventListener('click', this.start.bind(this));
 }
 
 App.prototype.start = function() {
-    // 키 이벤트
-    document.addEventListener('keydown', this.addKeyListener.bind(this));
-    // 아래로 내려가는 애니메이션
-    this.requestId = window.requestAnimationFrame(this.animate.bind(this))
+    if(!this.started) {
+        this.started = true;
+        // 키 이벤트
+        this.boundEventHandler = this.addKeyListener.bind(this);
+        document.addEventListener('keydown', this.boundEventHandler);
+        // 아래로 내려가는 애니메이션
+        this.requestId = window.requestAnimationFrame(this.animate.bind(this));
+    }
 }
 
 App.prototype.animate = function(now = 0) {
+    // drop 메서드 위에 써줘야 함
     this.requestId = window.requestAnimationFrame(this.animate.bind(this));
-    const time = this.time;
-    time.elapsed = now - time.start;
-    if(time.elapsed > time.level) {
-        time.start = now;
+    this.time.elapsed = now - this.time.start;
+    if(this.time.elapsed > this.time.level) {
+        this.time.start = now;
         this.drop();
     }
 }
@@ -63,8 +71,8 @@ App.prototype.addKeyListener = function(e) {
         let piece;
         while(this.board.vailDation(piece = this.moveBlock[KEYS.DOWN](this.piece))) {
             this.piece.moveBlock(piece);
-            this.drawScreen();
         }
+        this.drop();
     }
     else if(this.moveBlock[e.keyCode]) {
         const piece = this.moveBlock[e.keyCode].call(this, this.piece);
@@ -86,7 +94,8 @@ App.prototype.drop = function() {
     else {
         this.board.writeBoard(this.piece);
         this.board.removeLine();
-        if(this.makePiece()) {
+        this.score_
+        if(!this.makePiece()) {
             this.gameOver();
             return;
         }
@@ -101,19 +110,23 @@ App.prototype.drawScreen = function() {
 }
 
 /**
- * 블록을 생성하고
+ * 새로운 블록을 생성하고
  * 생성된 블록이 나오나자마 쌓여있는 블록과 겹치게 되면 return true를 해준다. (Game Over 조건)
  */
 App.prototype.makePiece = function() {
-    return !this.board.checkfline(this.piece = new Piece(this.context));
+    return this.board.checkfline(this.piece = new Piece(this.context));
 }
 
 /**
- * Game Over시 requestAnimationFrame을 정지시킨다.
+ * 
  */
 App.prototype.gameOver = function() {
     alert('Game Over');
     cancelAnimationFrame(this.requestId);
+    document.removeEventListener('keydown', this.boundEventHandler); // 이벤트를 제거 안해주면 중복으로 적용됨
+    this.board.clear();
+    this.board.initBoard();
+    this.started = false;
 }
 
 window.onload = function() {
--- js/board.js
+++ js/board.js
@@ -9,14 +9,18 @@ import {
     INNER_BLOCK_SIZE,
 } from './constant.js';
 
-export default function Board(context) {
+export default function Board(app) {
 
-    this.board = Array.from({length: ROWS}, ele => Array(COLS).fill(0));
-    this.context = context;
-
+    this.app = app;
+    this.context = app.context;
+    
+    this.initBoard();
     this.initBackground();
 }
 
+/**
+ * 테트리스의 그리드를 초기화해준다.
+ */
 Board.prototype.initBackground = function() {
     const that = this;
     // this.background = new Image();
@@ -39,12 +43,22 @@ Board.prototype.initBackground = function() {
     });
 }
 
+/**
+ * 테트리스의 백그라운드를 초기화해준다. (GameOver시 호출)
+ */
+Board.prototype.initBoard = function() {
+    this.board = Array.from({length: ROWS}, ele => Array(COLS).fill(0));
+}
+
 Board.prototype.vailDation = function(piece) {
     const that = this;
     return piece.shapeInfo.shape.every((e, dy) => {
         return e.every((value, dx) => {
             let x = piece.x + dx;
             let y = piece.y + dy;
+            // piece의 value가 0일경우 어디든 움직여도 상관이 없기 때문에 논리연산자를 이용해 true로 바꾸고 리턴
+            // piece의 value가 0이 아닐 경우 계산된 좌표의 숫자가 0일경우 true 리턴
+            // board의 y좌표가 undefined거나 x좌표가 undefined거나 쌓여있는 블록(1이상)이 있는 경우 false
             return !value || that.board[y] && that.board[y][x] === 0;
         });
     });
@@ -89,7 +103,7 @@ Board.prototype.writeBoard = function(piece) {
 Board.prototype.removeLine = function() {
     const that = this;
     this.board.forEach((e, dy) => {
-        const result = e.every((value, dx) => value > 0);
+        const result = e.every((value, dx) => value > 0); // 컬럼이 모두 0보다 크게되면
         if(result) {
             that.board.splice(dy, 1);
             that.board.unshift(Array(COLS).fill(0));
@@ -97,17 +111,27 @@ Board.prototype.removeLine = function() {
     })
 }
 
+/**
+ * 블록이 처음 생성되었을 때 이미 쌓여있는 블록과 겹쳐졌는지를 검사 (게임오버)
+ * return true: 겹쳐졌다 / return false: 안겹쳐졌다
+ * @param {} piece 
+ * @returns 
+ */
 Board.prototype.checkfline = function(piece) {
     const that = this;
     return piece.shapeInfo.shape.every((e, dy) => {
         return e.every((value, dx) => {
             let x = piece.x + dx;
             let y = piece.y + dy;
-            return !(piece.y == 0 && this.board[y] && this.board[y][x]);
+            // this.board[y][x]에서 0이나오면 겹치는게 아니더라도 false로 리턴해버리기 때문에 true로 바꿔줌
+            return piece.y == 0 && this.board[y] && !this.board[y][x];
         });
     });
 }
 
+/**
+ * 
+ */
 Board.prototype.clear = function() {
     this.context.clearRect(0, 0, WIDTH, HEIGHT);
     this.initBackground();
